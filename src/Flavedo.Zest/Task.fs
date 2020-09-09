938d54401f5a6ad65cd8925cfa47aa66efee7b29
Tatsuya MIDORIKAWA <midorikawa@any-one.jp>
2020-09-09 20:44:29 +0900

    💡Update

--- src/Flavedo.Zest/Task.fs
+++ src/Flavedo.Zest/Task.fs
@@ -4,16 +4,34 @@ open System
 open System.Threading
 open System.Threading.Tasks
 
-module Task =
-    let unwrap (task:System.Threading.Tasks.Task<'T>) =
-        task :> System.Threading.Tasks.Task
-        
 type internal NTask<'T> = System.Threading.Tasks.Task<'T>
 type internal NTask = System.Threading.Tasks.Task
 type internal VTask<'T> = System.Threading.Tasks.ValueTask<'T>
 type internal VTask = System.Threading.Tasks.ValueTask
 
-type Task () =
+[<AbstractClass; Sealed>]
+type Task private() =
+    static member internal unwrap (task:NTask<'T>) =
+        task :> NTask
+
+    static member internal unwrap (task:VTask<'T>) =
+        task.AsTask() :> NTask
+
+    static member run (action:unit->unit, ?token:CancellationToken) =
+        match token with
+        | Some t -> NTask.Run(action, t)
+        | None -> NTask.Run(action)
+        
+    static member run<'T> (f:unit->'T, ?token:CancellationToken) =
+        match token with
+        | Some t -> NTask.Run(f, t)
+        | None -> NTask.Run(f)
+        
+    static member delay (time:int<millisec>, ?token:CancellationToken) =
+        match token with
+        | Some t -> NTask.Delay(int time, t)
+        | None -> NTask.Delay(int time)
+
     static member waitAll ([<ParamArray>]tasks, token:CancellationToken option) =
         match token with
         | Some t -> Task.WaitAll(tasks, t)
@@ -119,6 +137,51 @@ type Task () =
         (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result, task7.Result, task8.Result)
         
     static member await<'T1, 'T2, 'T3, 'T4, 'T5, 'T6, 'T7, 'T8, 'T9> (task1:NTask<'T1>, task2:NTask<'T2>, task3:NTask<'T3>, task4:NTask<'T4>, task5:NTask<'T5>, task6:NTask<'T6>, task7:NTask<'T7>, task8:NTask<'T8>, task9:NTask<'T9>, ?token:CancellationToken) =
+        let tasks = [| Task.unwrap task1; Task.unwrap task2; Task.unwrap task3; Task.unwrap task4; Task.unwrap task5; Task.unwrap task6; Task.unwrap task7; Task.unwrap task8; Task.unwrap task9; |]
+        Task.waitAll(tasks, token)
+        (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result, task7.Result, task8.Result, task9.Result)
+        
+    static member await<'T> (task:VTask<'T>, ?token:CancellationToken) =
+        let task' = Task.unwrap task
+        Task.waitAll([| task' |], token)
+        task.Result
+
+    static member await<'T1, 'T2> (task1:VTask<'T1>, task2:VTask<'T2>, ?token:CancellationToken) =
+        let tasks = [| Task.unwrap task1; Task.unwrap task2; |]
+        Task.waitAll(tasks, token)
+        (task1.Result, task2.Result)
+        
+    static member await<'T1, 'T2, 'T3> (task1:VTask<'T1>, task2:VTask<'T2>, task3:VTask<'T3>, ?token:CancellationToken) =
+        let tasks = [| Task.unwrap task1; Task.unwrap task2; Task.unwrap task3; |]
+        Task.waitAll(tasks, token)
+        (task1.Result, task2.Result, task3.Result)
+            
+    static member await<'T1, 'T2, 'T3, 'T4> (task1:VTask<'T1>, task2:VTask<'T2>, task3:VTask<'T3>, task4:VTask<'T4>, ?token:CancellationToken) =
+        let tasks = [| Task.unwrap task1; Task.unwrap task2; Task.unwrap task3; Task.unwrap task4; |]
+        Task.waitAll(tasks, token)
+        (task1.Result, task2.Result, task3.Result, task4.Result)
+            
+    static member await<'T1, 'T2, 'T3, 'T4, 'T5> (task1:VTask<'T1>, task2:VTask<'T2>, task3:VTask<'T3>, task4:VTask<'T4>, task5:VTask<'T5>, ?token:CancellationToken) =
+        let tasks = [| Task.unwrap task1; Task.unwrap task2; Task.unwrap task3; Task.unwrap task4; Task.unwrap task5; |]
+        Task.waitAll(tasks, token)
+        (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result)
+            
+    static member await<'T1, 'T2, 'T3, 'T4, 'T5, 'T6> (task1:VTask<'T1>, task2:VTask<'T2>, task3:VTask<'T3>, task4:VTask<'T4>, task5:VTask<'T5>, task6:VTask<'T6>, ?token:CancellationToken) =
+        let tasks = [| Task.unwrap task1; Task.unwrap task2; Task.unwrap task3; Task.unwrap task4; Task.unwrap task5; Task.unwrap task6; |]
+        Task.waitAll(tasks, token)
+        (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result)
+            
+    static member await<'T1, 'T2, 'T3, 'T4, 'T5, 'T6, 'T7> (task1:VTask<'T1>, task2:VTask<'T2>, task3:VTask<'T3>, task4:VTask<'T4>, task5:VTask<'T5>, task6:VTask<'T6>, task7:VTask<'T7>, ?token:CancellationToken) =
+        let tasks = [| Task.unwrap task1; Task.unwrap task2; Task.unwrap task3; Task.unwrap task4; Task.unwrap task5; Task.unwrap task6; Task.unwrap task7; |]
+        Task.waitAll(tasks, token)
+        (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result, task7.Result)
+            
+    static member await<'T1, 'T2, 'T3, 'T4, 'T5, 'T6, 'T7, 'T8> (task1:VTask<'T1>, task2:VTask<'T2>, task3:VTask<'T3>, task4:VTask<'T4>, task5:VTask<'T5>, task6:VTask<'T6>, task7:VTask<'T7>, task8:VTask<'T8>, ?token:CancellationToken) =
+        let tasks = [| Task.unwrap task1; Task.unwrap task2; Task.unwrap task3; Task.unwrap task4; Task.unwrap task5; Task.unwrap task6; Task.unwrap task7; Task.unwrap task8; |]
+        Task.waitAll(tasks, token)
+        (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result, task7.Result, task8.Result)
+            
+    static member await<'T1, 'T2, 'T3, 'T4, 'T5, 'T6, 'T7, 'T8, 'T9> (task1:VTask<'T1>, task2:VTask<'T2>, task3:VTask<'T3>, task4:VTask<'T4>, task5:VTask<'T5>, task6:VTask<'T6>, task7:VTask<'T7>, task8:VTask<'T8>, task9:VTask<'T9>, ?token:CancellationToken) =
         let tasks = [| Task.unwrap task1; Task.unwrap task2; Task.unwrap task3; Task.unwrap task4; Task.unwrap task5; Task.unwrap task6; Task.unwrap task7; Task.unwrap task8; Task.unwrap task9; |]
         Task.waitAll(tasks, token)
         (task1.Result, task2.Result, task3.Result, task4.Result, task5.Result, task6.Result, task7.Result, task8.Result, task9.Result)
--- src/Samples/Flavedo.Zest.Samples/Program.fs
+++ src/Samples/Flavedo.Zest.Samples/Program.fs
@@ -1,19 +1,46 @@
-﻿open System
-open System.Threading.Tasks
+﻿// open Flavedo.Zest
+
+// let t1 () = Task.run(fun () -> 
+//     printfn "start1"
+//     Task.await (Task.delay(1000<millisec>))
+//     printfn "### 1"
+//     Task.await (Task.delay(1000<millisec>))
+//     printfn "### 1"
+//     Task.await (Task.delay(1000<millisec>))
+//     printfn "### 1"
+//     Task.await (Task.delay(1000<millisec>))
+//     printfn "finish1" )
+
+// let t2 () = Task.run(fun () -> 
+//     printfn "start2"
+//     Task.await (Task.delay(800<millisec>))
+//     printfn "### 2"
+//     Task.await (Task.delay(800<millisec>))
+//     printfn "### 2"
+//     Task.await (Task.delay(800<millisec>))
+//     printfn "### 2"
+//     Task.await (Task.delay(800<millisec>))
+//     printfn "finish2" )
+
+// Task.await(t1(), t2())
+
+
+// open type Flavedo.Zest.Task
+
 open Flavedo.Zest
 
+let vt1 () = Task.run(fun () -> 
+    printfn "start1"
+    Task.await (Task.delay(1000<millisec>))
+    printfn "finish1" 
+    500)
 
-type internal NTask = System.Threading.Tasks.Task
+let vt2 () = Task.run(fun () -> 
+    printfn "start2"
+    Task.await (Task.delay(800<millisec>))
+    printfn "finish2"
+    "vtask2")
 
-let task () =
-    NTask.Run(fun () -> 
-        printfn "start"
-        let t = NTask.Delay(3000)
-        Task.WaitAll(t)
-        printfn "finish")
+let (v1, v2) = Task.await(vt1(), vt2())
+printfn "v1= %d, v2= %s" v1 v2
 
-let t = task()
-printfn "sample1"
-printfn "sample2"
-printfn "sample3"
-Task.await t
